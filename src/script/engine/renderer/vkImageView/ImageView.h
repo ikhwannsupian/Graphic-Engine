b8d7736fd7eaf9ba272d7c73c4ba9bfaf8461a2d
wan <ikhwannsupian@gmail.com>
2026-09-07 23:32:48 +0800

    init Vulkan Image View

---
+++ src/script/engine/renderer/vkImageView/ImageView.h
@@ -0,0 +1,10 @@
+#ifndef VKIMAGE_H
+#define VKIMAGE_H
+#include <vulkan/vulkan.hpp>
+#include <vector>
+
+class VulkanImageView
+{
+
+};
+#endif
---
+++ src/script/engine/renderer/vkImageView/imageView.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include <vector>
+
+#include <vulkan/vulkan.hpp>
+#include <vulkan/vulkan.h>
+#include <SDL3/SDL_vulkan.h>
+#include "imageView.h"
